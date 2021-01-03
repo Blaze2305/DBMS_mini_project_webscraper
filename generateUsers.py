1edfe1ab18fba8f5fc1c4b0514afbb0bf7462757
Pranav <prajpran@gmail.com>
2021-01-04 02:14:13 +0530

    .

--- generateUsers.py
+++ generateUsers.py
@@ -67,7 +67,7 @@ def generateUser():
 			"Department": dept,
 			"Year" : year,
 			"Section" : section,
-			"Date of Birth" : dob,
+			"DOB" : dob,
 			"Contact":phone,
 			"Type":"Student",
 			"PhotoUrl":"https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light"
